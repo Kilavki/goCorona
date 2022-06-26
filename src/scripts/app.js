960ee78c5f039cdc3b19a83e1496758a0d8295a5
kilavki <kilavki@gmail.com>
2022-06-26 23:30:52 +0300

    ed: header hamburger

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,3 +5,14 @@ flsFunctions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper(...);
+
+// Бургер
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
+}
