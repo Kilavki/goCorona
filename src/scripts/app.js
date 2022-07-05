47bddf1aa01fbafe1154fc1731bf57a15c7a75ed
kilavki <kilavki@gmail.com>
2022-07-05 22:53:39 +0300

    add: js scroll

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,13 +6,51 @@ flsFunctions.isWebp();
 
 // const swiper = new Swiper(...);
 
+
+
+
+
 // Бургер
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu__body');
 
 if (iconMenu) {
 	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock');
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
 	})
+}
+
+// Плавная прокрутка
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageXOffset - document.querySelector('.header').offsetHeight;
+			console.log(gotoBlockValue);
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: 'smooth'
+			});
+
+			e.preventDefault();
+		}
+	}
 }
